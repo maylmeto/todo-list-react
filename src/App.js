01ffbb563ef3a094fd3a0abd03c31d4b4fb0db85
Mayl Meto <maylme@protonmail.com>
2020-05-22 08:05:29 +0545

    Comments regarding rendering TodoInput and TodoList

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
+// TodoInput, TodoList will be rendered in the App.js
 function App() {
   return (
     <div className="App">
